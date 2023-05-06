a0f0abe0bd7081a3025fa49e835f2cdbdbd6dc15
SashaBeliiSR <117167497+SashaBeliiSR@users.noreply.github.com>
2023-05-06 13:13:22 +0300

    Add files via upload

---
+++ db-games.js
@@ -0,0 +1,24 @@
+var params = location.search.substring(1).split("&");
+var result = "";
+for(i = 0; i < params.length; i ++) result += ":" + params[i].split("=")[1];
+result = result.substring(1);
+console.log(result);
+
+
+const baseUrl = 'http://6455f3055f9a4f2361374d32.mockapi.io/api/v1/games?id=' + result;
+console.log('JSON read is runned!');
+const xhr = new XMLHttpRequest()
+
+xhr.open('GET', baseUrl)
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+xhr.responseType = 'json'
+
+xhr.onload = function() {
+	console.log(xhr.response)
+	let = Obj = xhr.response;
+	console.log('title is=>' + Obj.title);
+}
+
+
+xhr.send()
